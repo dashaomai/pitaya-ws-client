c2a20016246e3838ec600aeebb53597d573c65ae
Buddy Jiang <dashaomai@gmail.com>
2024-07-16 09:29:57 +0800

    feat: 将 pomelo client 中 str encode/decode 内容替换为标准的 utf-8 编/解码组件。

--- lib/protocol.ts
+++ lib/protocol.ts
@@ -33,51 +33,16 @@ export enum MessageTypes {
 }
 
 const ByteArray = Uint8Array
+const utf8Encoder = new TextEncoder()
+const utf8Decoder = new TextDecoder('UTF-8')
 
 class Protocol {
     static strencode(str: any) {
-        let byteArray = new ByteArray(str.length * 3);
-        let offset = 0;
-        for (let i = 0; i < str.length; i++) {
-            let charCode = str.charCodeAt(i);
-            let codes: any;
-            if (charCode <= 0x7f) {
-                codes = [charCode];
-            } else if (charCode <= 0x7ff) {
-                codes = [0xc0 | (charCode >> 6), 0x80 | (charCode & 0x3f)];
-            } else {
-                codes = [0xe0 | (charCode >> 12), 0x80 | ((charCode & 0xfc0) >> 6), 0x80 | (charCode & 0x3f)];
-            }
-            for (let j = 0; j < codes.length; j++) {
-                byteArray[offset] = codes[j];
-                ++offset;
-            }
-        }
-        let _buffer = new ByteArray(offset);
-        copyArray(_buffer, 0, byteArray, 0, offset);
-        return _buffer;
-    };
+        return utf8Encoder.encode(str)
+    }
     static strdecode(buffer: any) {
-        let bytes = new ByteArray(buffer);
-        let array: Array<number> = [];
-        let offset = 0;
-        let charCode = 0;
-        let end = bytes.length;
-        while (offset < end) {
-            if (bytes[offset] < 128) {
-                charCode = bytes[offset];
-                offset += 1;
-            } else if (bytes[offset] < 224) {
-                charCode = ((bytes[offset] & 0x3f) << 6) + (bytes[offset + 1] & 0x3f);
-                offset += 2;
-            } else {
-                charCode = ((bytes[offset] & 0x0f) << 12) + ((bytes[offset + 1] & 0x3f) << 6) + (bytes[offset + 2] & 0x3f);
-                offset += 3;
-            }
-            array.push(charCode);
-        }
-        return String.fromCharCode.apply(null, array);
-    };
+        return utf8Decoder.decode(buffer)
+    }
 }
 
 
@@ -94,7 +59,7 @@ class PackageProtocol {
             copyArray(buffer, index, body, 0, length);
         }
         return buffer;
-    };
+    }
 
     static decode = function (buffer: Uint8Array) {
         let offset = 0;
